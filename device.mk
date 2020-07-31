736c0604af4e55a67d179b55a2b60e068813d666
Shekhar <meshekharbehera613@gmail.com>
2020-07-31 11:57:08 +0530

    begonia : adding DEVICE_PATH := device/redmi/begonia
    Signed-off-by : Flamefusion <meshekharbehera613@gmail.com>

--- device.mk
+++ device.mk
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+ DEVICE_PATH := device/redmi/begonia
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
