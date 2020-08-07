7726a0d2f4a24b8c2c1a25c20631f4d1402163ef
Flamefusion <meshekharbehera613@gmail.com>
2020-08-07 15:46:33 +0530

    [SQUASH]begonia: build XiaomiParts
    some extra prebuilt features from ZenParts. I know there are lot of
    commits out
    there for xiaomiparts still i liked the things from ZenParts so i ported
    it as XiaomiParts
    Squashed commits:-
    *Add Ambient Display Gestures
    *Implemented FPS info in xiaomiparts
    *Added Spectrum Profiles support in xiaomiparts
    *implementation of keyhandler for xiaomiparts
    *added string for category_gesture
    *Fix illegal start of statement
    *Added Dirac QS Tile
    *Added Dirac Service
    *Removed selinux switch option(garbage)
    *Remove headphone and microphone gain for now as we dont have sound control
    support in our kernel source
    *changed the look of XiaomiParts
    -updated the xiaomi logo                                 - -updated preview.jpg
    *add separate xiaomi parts initilaization in rootdir
    Signed-off-by: Flamefusion <meshekharbehera613@gmail.com>
    
    Co-authored-by: Rizky Benggolo <travarilo@gmail.com>
    Co-authored-by: mprabhat774 <mprabhat774@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -138,14 +138,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.xiaomi_parts.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.xiaomi_parts.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.modem.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
@@ -239,6 +231,14 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.mt6785
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.xiaomi_parts.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.xiaomi_parts.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
