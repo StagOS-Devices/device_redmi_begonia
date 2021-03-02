65086b89a6d2cded2a2387d9f46a604d209661a6
SahilSonar <sss.sonar2003@gmail.com>
2021-03-02 05:12:23 +0100

    begonia: Update build fingerprint/desc from sunfish
    Imported from sunfish-user 11 RQ2A.210305.006 7119741 release-keys
    
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- stag_begonia.mk
+++ stag_begonia.mk
@@ -39,10 +39,10 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
+BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ2A.210305.006/7119741:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="begonia-user 10 QP1A.190711.020 V11.0.2.0.QGGEUXM release-keys" \
+    PRIVATE_BUILD_DESC="sunfish-user 11 RQ2A.210305.006 7119741 release-keys" \
     PRODUCT_NAME="begonia" \
     TARGET_DEVICE="begonia"
 
